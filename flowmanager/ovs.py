be7c96f07f89d806456e6593b9c7e9f3b0b0640e
Jon Castro <castro.jon@gmail.com>
2017-11-09 15:48:03 +1100

    check if ovs is running locally

--- flowmanager/ovs.py
+++ flowmanager/ovs.py
@@ -1,3 +1,7 @@
 class OVS(Switch):
     def __init__(self, props):
         Switch.__init__(self, props)
+
+        # if IP address and user is not given
+        # then we need to assume OVS is running locally
+        self.execute_local = not props.get('ip') and not props.get('user')
