109c99b515ff499c08649ee308604c0393f68f16
Jon Castro <castro.jon@gmail.com>
2017-11-09 13:33:45 +1100

    add first contrusction of topology and basic clases

---
+++ flowmanager/noviflow.py
@@ -0,0 +1,3 @@
+class Noviflow(Switch):
+    def __init__(self, props):
+        Switch.__init__(self, props)
---
+++ flowmanager/ovs.py
@@ -0,0 +1,3 @@
+class OVS(Switch):
+    def __init__(self, props):
+        Switch.__init__(self, props)
