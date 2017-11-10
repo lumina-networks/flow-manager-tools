aee09b55386c35765fd3eddeeb4ee3828a73e1b8
Jon Castro <castro.jon@gmail.com>
2017-11-10 17:17:51 +1100

    add calculated groups first draft

---
+++ flowmanager/group.py
@@ -0,0 +1,50 @@
+import logging
+
+class Group(object):
+
+    def __init__(self, node, groupid):
+        self.node = node
+        self.groupid = groupid
+        self.of_config = None
+        self.of_operational = None
+        self.switch = None
+        self.fm = None
+        self.calculated = False
+
+    def add_of_config(self, group):
+        self.of_config = group
+
+    def add_of_operational(self, group):
+        self.of_operational = group
+
+    def add_switch(self, group):
+        self.switch = group
+
+    def add_fm(self, group):
+        self.fm = group
+
+    def mark_as_calculated(self):
+        self.calculated = True
+
+    def check(self):
+        if self.of_config and not self.switch:
+            print "ERROR: group {} is not runnig in the switch. {}".format(self.groupid, self._get_info_msg())
+        elif self.of_config and not self.of_operational:
+            print "ERROR: group {} not found in operational datastore. {}".format(self.groupid, self._get_info_msg())
+        elif self.of_config and not self.fm:
+            print "ERROR: group {} is not being monitored. {}".format(self.groupid, self._get_info_msg())
+        elif self.of_config and not self.calculated:
+            print "ERROR: group {} not found in calculated groups. {}".format(self.groupid, self._get_info_msg())
+        elif not self.of_config and self.switch:
+            print "ERROR: group {} runnig in switch but not configured. {}".format(self.groupid, self._get_info_msg())
+        elif not self.of_config and self.of_operational:
+            print "ERROR: group {} found operational datastore but not in configuration. {}".format(self.groupid, self._get_info_msg())
+        elif not self.of_config and not self.of_operational and not self.switch and self.fm:
+            print "ERROR: group {} monitored but not running neither configured. {}".format(self.groupid, self._get_info_msg())
+        elif not self.of_config and self.calculated:
+            print "ERROR: group {} calculated but not configured. {}".format(self.groupid, self._get_info_msg())
+        else:
+            return True
+
+    def _get_info_msg(self):
+        return "of config ({}), of operational ({}), switch ({}), monitored ({}), calculated ({})".format(self.of_config != None, self.of_operational != None, self.switch != None, self.fm != None, self.calculated)
--- flowmanager/noviflow.py
+++ flowmanager/noviflow.py
@@ -1,3 +1,4 @@
+import re
 from flowmanager.switch import Switch
 from flowmanager.ssh import NoviflowSSH
 
@@ -8,11 +9,12 @@ class Noviflow(Switch):
 
     def reboot(self):
         ssh = self._get_ssh()
-        if ssh.create_session():
-            if ssh.execute_command('set status switch reboot',prompt="[all/noentries/nopipeline/none]"):
-                if ssh.execute_command('noentries',prompt="(y/n)"):
-                    ssh.execute_command('y', prompt=None, eof=True)
-                    return True
+        if not ssh.create_session():
+            return None
+        if ssh.execute_command('set status switch reboot',prompt="[all/noentries/nopipeline/none]"):
+            if ssh.execute_command('noentries',prompt="(y/n)"):
+                ssh.execute_command('y', prompt=None, eof=True)
+                return True
 
 
     def break_gateway(self, seconds=0):
@@ -20,10 +22,11 @@ class Noviflow(Switch):
 
     def delete_groups(self):
         ssh = self._get_ssh()
-        if ssh.create_session():
-            if ssh.execute_command('del config group groupid all'):
-                ssh.close()
-                return True
+        if not ssh.create_session():
+            return None
+        if ssh.execute_command('del config group groupid all'):
+            ssh.close()
+            return True
 
     def delete_flows(self):
         raise Exception('delete flows is not implemented by this switch {}'.format(self.name))
@@ -32,7 +35,45 @@ class Noviflow(Switch):
         raise Exception('get flows method is not implemented by this switch {}'.format(self.name))
 
     def get_groups(self):
-        raise Exception('get flows method is not implemented by this switch {}'.format(self.name))
+        ssh = self._get_ssh()
+        if not ssh.create_session():
+            return None
+        text_groups = ssh.execute_command('show stats group groupid all')
+        if not text_groups:
+            return None
+
+        groupIdRegex = re.compile(r'Group id:\s*(\d+)', re.IGNORECASE)
+        packetCountRegex = re.compile(r'Reference count:\s*\d+\s*\S\s+Packet count:\s*(\d+)', re.IGNORECASE)
+        byteCountRegex = re.compile(r'Byte count:\s*(\d+)', re.IGNORECASE)
+
+        groups = {}
+        current_group = None
+        for line in text_groups.splitlines():
+            match = groupIdRegex.findall(line)
+            if match:
+                current_group = {}
+                groups[int(match[0])] = current_group
+                continue
+            elif current_group is None:
+                continue
+
+            match = packetCountRegex.findall(line)
+            if match:
+                current_group['packets']=match[0]
+                if 'bytes' in current_group:
+                    current_group = None
+                continue
+
+            match = byteCountRegex.findall(line)
+            if match:
+                current_group['bytes']=match[0]
+                if 'packets' in current_group:
+                    current_group = None
+                continue
+
+        ssh.close()
+        return groups
+
 
     def get_controllers_role(self):
         raise Exception('get controllers method is not implemented by this switch {}'.format(self.name))
