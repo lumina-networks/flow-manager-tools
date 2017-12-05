8679c66a3e99054b89d22544e118812377faea60
Jon Castro <castro.jon@gmail.com>
2017-12-05 15:08:30 +1100

    intitialize state to unknown for non OVS/Noviflow switches

--- flowmanager/ovs.py
+++ flowmanager/ovs.py
@@ -4,6 +4,7 @@ class OVS(Switch):
     def __init__(self, props, expected=False):
         Switch.__init__(self, props, expected)
 
+        self.type = 'ovs'
         # if IP address and user is not given
         # then we need to assume OVS is running locally
         self.execute_local = not props.get('ip') and not props.get('user')
