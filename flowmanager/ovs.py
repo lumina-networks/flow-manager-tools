45665c6b233f636615e150de385b1c497d12e6b3
qasimraz <qasimraza788@gmail.com>
2017-12-14 09:55:05 +1100

    added class-level docstrings for OVS and novi

--- flowmanager/ovs.py
+++ flowmanager/ovs.py
@@ -7,6 +7,13 @@ from flowmanager.ssh import SSH
 
 
 class OVS(Switch):
+
+    """
+
+    Inherits from Switch and Implements Open vSwitch specific methods
+
+    """
+
     def __init__(self, props, expected=False):
         Switch.__init__(self, props, expected)
 
@@ -14,7 +21,8 @@ class OVS(Switch):
         # if IP address and user is not given
         # then we need to assume OVS is running locally
         self.execute_local = not props.get('ip') and not props.get('user')
-        self.ssh = None if not self.execute_local else SSH(self.ip, self.user, self.port, self.port, self.password)
+        self.ssh = None if not self.execute_local else SSH(
+            self.ip, self.user, self.port, self.port, self.password)
 
     def _execute_commands(self, commands):
         for command in commands:
@@ -28,25 +36,29 @@ class OVS(Switch):
         else:
             return subprocess.check_output(command, shell=True)
 
-
     def reboot(self):
-        output = self._execute_command("sudo ovs-vsctl get-controller {}".format(self.name))
-        controllersRegex = re.compile(r'(tcp:\d+\.\d+\.\d+\.\d+\:\d+)', re.IGNORECASE)
+        output = self._execute_command(
+            "sudo ovs-vsctl get-controller {}".format(self.name))
+        controllersRegex = re.compile(
+            r'(tcp:\d+\.\d+\.\d+\.\d+\:\d+)', re.IGNORECASE)
         match = controllersRegex.findall(output)
         if not match:
             logger.error("cannot get controllers for {}".format(self.name)
             return False
-            controllers = ' '.join(match)
-            self._execute_command("sudo ovs-vsctl del-controller {}".format(self.name))
+            controllers=' '.join(match)
+            self._execute_command(
+                "sudo ovs-vsctl del-controller {}".format(self.name))
             self.delete_flows()
             self.delete_groups()
             time.sleep(5)
-            self._execute_command("sudo ovs-vsctl set-controller {} {}".format(self.name, controllers), shell=True)
+            self._execute_command(
+                "sudo ovs-vsctl set-controller {} {}".format(self.name, controllers), shell=True)
             return True
 
     def break_gateway(self, seconds=0):
-        seconds = int(seconds)
-        logging.debug("INFO: trying to break connectivity to the switch %s switch", self.name)
+        seconds=int(seconds)
+        logging.debug(
+            "INFO: trying to break connectivity to the switch %s switch", self.name)
         if 'disable_gw' not in self.props or len(self.props['disable_gw']) <= 0 or 'enable_gw' not in self.props or len(self.props['enable_gw']) <= 0:
             print "ERROR: enable or disable gw commands not found in switch {} switch".format(self.name)
             return False
@@ -62,70 +74,80 @@ class OVS(Switch):
         return self._execute_command("sudo ovs-ofctl del-groups {} --protocol=Openflow13".format(self.name))
 
     def get_flows(self):
-        output = self._execute_command("sudo ovs-ofctl dump-flows {} --protocol=Openflow13".format(self.name))
+        output=self._execute_command(
+            "sudo ovs-ofctl dump-flows {} --protocol=Openflow13".format(self.name))
         if not output:
             return None
 
-        regex = re.compile(r'(cookie=.*)', re.IGNORECASE)
-        regexvalues = re.compile(r'cookie=(0[xX][0-9a-fA-F]+),.*table=(\d+),.*n_packets=(\d+),.*n_bytes=(\d+)', re.IGNORECASE)
+        regex=re.compile(r'(cookie=.*)', re.IGNORECASE)
+        regexvalues=re.compile(
+            r'cookie=(0[xX][0-9a-fA-F]+),.*table=(\d+),.*n_packets=(\d+),.*n_bytes=(\d+)', re.IGNORECASE)
 
-        flows = []
-        flowid = None
+        flows=[]
+        flowid=None
         for linematch in regex.finditer(output):
-            line = linematch.group(1)
+            line=linematch.group(1)
             for match in regexvalues.finditer(line):
-                flow = {}
-                flow['id'] = flowid
-                flow['cookie'] = int(match.group(1), 16)
-                flow['table'] = match.group(2)
-                flow['packets'] = match.group(3)
-                flow['bytes'] = match.group(4)
+                flow={}
+                flow['id']=flowid
+                flow['cookie']=int(match.group(1), 16)
+                flow['table']=match.group(2)
+                flow['packets']=match.group(3)
+                flow['bytes']=match.group(4)
                 flows.append(flow)
 
         return flows
 
     def get_groups(self):
-        output = self._execute_command("sudo ovs-ofctl dump-group-stats {} --protocol=Openflow13".format(self.name))
+        output=self._execute_command(
+            "sudo ovs-ofctl dump-group-stats {} --protocol=Openflow13".format(self.name))
         if not output:
             return None
-        regex = re.compile(r'(group_id=.*)', re.IGNORECASE)
-        regexvalues = re.compile(r'group_id=(\d+),duration=[\d]*.[\d]*s,ref_count=[\d]*,packet_count=(\d+),byte_count=(\d+)', re.IGNORECASE)
+        regex=re.compile(r'(group_id=.*)', re.IGNORECASE)
+        regexvalues=re.compile(
+            r'group_id=(\d+),duration=[\d]*.[\d]*s,ref_count=[\d]*,packet_count=(\d+),byte_count=(\d+)', re.IGNORECASE)
 
-        groups = []
+        groups=[]
         for linematch in regex.finditer(output):
-            line = linematch.group(1)
+            line=linematch.group(1)
             for match in regexvalues.finditer(line):
-                group = {}
-                group['id'] = match.group(1)
-                group['packets'] = match.group(2)
-                group['bytes'] = match.group(3)
+                group={}
+                group['id']=match.group(1)
+                group['packets']=match.group(2)
+                group['bytes']=match.group(3)
                 groups.append(group)
 
         return groups
 
 
     def get_controllers_role(self):
-        controllers = self._execute_command("sudo ovs-vsctl  get Bridge {} controller".format(self.name))
-	    logging.debug("DEBUG: Controllers UUID received for switch %s are : %s", self.name,controllers)
+        controllers=self._execute_command(
+            "sudo ovs-vsctl  get Bridge {} controller".format(self.name))
+	    logging.debug(
+	        "DEBUG: Controllers UUID received for switch %s are : %s", self.name, controllers)
         if not controllers:
             return None
-        regex = re.compile(r'([0-9a-fA-F-]+)', re.IGNORECASE)
-        roles = []
+        regex=re.compile(r'([0-9a-fA-F-]+)', re.IGNORECASE)
+        roles=[]
         for line in controllers.split(','):
             for match in regex.finditer(line):
                 if match:
-                    controller = match.group(1)
-                    role = self._execute_command("sudo ovs-vsctl  get controller {} role".format(controller))
-                    logging.debug("DEBUG: Controller with uuid %s on switch %s has role : %s", controller,self.name,role)
+                    controller=match.group(1)
+                    role=self._execute_command(
+                        "sudo ovs-vsctl  get controller {} role".format(controller))
+                    logging.debug(
+                        "DEBUG: Controller with uuid %s on switch %s has role : %s", controller, self.name, role)
                     if role:
                         roles.append(role.strip('\n'))
         return roles
 
     def shutdown_port(self, port):
-        shut = self._execute_command("sudo ovs-ofctl -O OpenFlow13 mod-port {} {} down".format(self.name, port))
+        shut=self._execute_command(
+            "sudo ovs-ofctl -O OpenFlow13 mod-port {} {} down".format(self.name, port))
 
     def start_port(self, port):
-        start = self._execute_command("sudo ovs-ofctl -O OpenFlow13 mod-port {} {} up".format(self.name, port))
+        start=self._execute_command(
+            "sudo ovs-ofctl -O OpenFlow13 mod-port {} {} up".format(self.name, port))
 
     def restart_port(self, port, seconds=0):
         if not self.shutdown_port(port):
@@ -134,9 +156,11 @@ class OVS(Switch):
         return self.start_port(port)
 
     def list_ports(self):
-        list = self._execute_command("sudo ovs-vsctl list-ports {}".format(self.name))
+        list=self._execute_command(
+            "sudo ovs-vsctl list-ports {}".format(self.name))
         print list
 
     def port_status(self, port):
-        status = self._execute_command("sudo ovs-ofctl -O OpenFlow13 dump-ports-desc {} {}".format(self.name, port))
+        status=self._execute_command(
+            "sudo ovs-ofctl -O OpenFlow13 dump-ports-desc {} {}".format(self.name, port))
         print status
