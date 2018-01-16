1bffea27dc90022e065b32cb69c0bbac387a5d24
qasimraz <qasimraza788@gmail.com>
2018-01-16 14:51:50 +1100

    Changed print to logging

--- flowmanager/switch.py
+++ flowmanager/switch.py
@@ -31,11 +31,11 @@ class Switch(object):
         self.password = None if not props.get('password') else props['password']
         self.ip = None if not props.get('ip') else props['ip']
         self.port = 22 if not props.get('port') else props['port']
-        self.links = {}
-        self.flows = {}
-        self.flows_by_name = {}
-        self.flows_by_id = {}
-        self.groups = {}
+        self.links = %s
+        self.flows = %s
+        self.flows_by_name = %s
+        self.flows_by_id = %s
+        self.groups = %s
         logging.debug('SWITCH: created switch %s(%s), type %s, ip %s, dpid %s', self.name, self.openflow_name, self.type, self.ip, props['dpid'])
 
     def get_link(self, source, expected_dst=None):
@@ -58,7 +58,7 @@ class Switch(object):
         if not cookie and (not table or not name):
             raise Exception('cookie or table and name is mandatory')
 
-        flow_name = "table/{}/name/{}".format(table, name) if table is not None and name is not None else None
+        flow_name = "table/%s/name/%s", table, name) if table is not None and name is not None else None
         flow_fm_id = str(get_flow_id(cookie)) if cookie is not None else None
 
         current_flow = self.flows_by_name[flow_name] if flow_name and flow_name in self.flows_by_name else None
@@ -80,48 +80,48 @@ class Switch(object):
     def check(self, should_be_up=True, validate_sr=True):
         logging.debug("SWITCH: checking switch %s(%s) , connected %s, of topology %s, sr topology %s",self.name, self.openflow_name, self.found_connected, self.found_openflow_topology, self.found_sr_topology)
         if (not self.expected):
-            print "ERROR: unexpected switch {}({}).".format(self.name, self.openflow_name)
+            logging.error("unexpected switch %s(%s).", self.name, self.openflow_name)
         elif (should_be_up and not self.found_connected):
-            print "ERROR: switch {}({}) not connected.".format(self.name, self.openflow_name)
+            logging.error("switch %s(%s) not connected.", self.name, self.openflow_name)
         elif (not should_be_up and self.found_connected):
-            print "ERROR: switch {}({}) should NOT be connected.".format(self.name, self.openflow_name)
+            logging.error("switch %s(%s) should NOT be connected.", self.name, self.openflow_name)
         elif (should_be_up and not self.found_openflow_topology):
-            print "ERROR: switch {}({}) not found in topology.".format(self.name, self.openflow_name)
+            logging.error("switch %s(%s) not found in topology.", self.name, self.openflow_name)
         elif (not should_be_up and self.found_openflow_topology):
-            print "ERROR: switch {}({}) should NOT be in topology.".format(self.name, self.openflow_name)
+            logging.error("switch %s(%s) should NOT be in topology.", self.name, self.openflow_name)
         elif (validate_sr and should_be_up and not self.found_sr_topology):
-            print "ERROR: switch {}({}) not found in sr topology.".format(self.name, self.openflow_name)
+            logging.error("switch %s(%s) not found in sr topology.", self.name, self.openflow_name)
         elif (validate_sr and not should_be_up and self.found_sr_topology):
-            print "ERROR: switch {}({}) should NOT be in sr topology.".format(self.name, self.openflow_name)
+            logging.error("switch %s(%s) should NOT be in sr topology.", self.name, self.openflow_name)
         else:
             return True
 
     def reboot(self):
-        raise Exception('reboot method is not implemented by this switch {}'.format(self.name))
+        raise Exception('reboot method is not implemented by this switch %s', self.name))
 
     def break_gateway(self, seconds=0):
-        raise Exception('break method is not implemented by this switch {}'.format(self.name))
+        raise Exception('break method is not implemented by this switch %s', self.name))
 
     def delete_groups(self):
-        raise Exception('delete groups method is not implemented by this switch {}'.format(self.name))
+        raise Exception('delete groups method is not implemented by this switch %s', self.name))
 
     def delete_flows(self):
-        raise Exception('delete flows is not implemented by this switch {}'.format(self.name))
+        raise Exception('delete flows is not implemented by this switch %s', self.name))
 
     def get_flows(self):
-        raise Exception('get flows method is not implemented by this switch {}'.format(self.name))
+        raise Exception('get flows method is not implemented by this switch %s', self.name))
 
     def get_groups(self):
-        raise Exception('get groups method is not implemented by this switch {}'.format(self.name))
+        raise Exception('get groups method is not implemented by this switch %s', self.name))
 
     def get_controllers_role(self):
-        raise Exception('get controllers method is not implemented by this switch {}'.format(self.name))
+        raise Exception('get controllers method is not implemented by this switch %s', self.name))
 
     def shutdown_port(self, port):
-        raise Exception('shutdown port method is not implemented by this switch {}'.format(self.name))
+        raise Exception('shutdown port method is not implemented by this switch %s', self.name))
 
     def start_port(self, port):
-        raise Exception('start port method is not implemented by this switch {}'.format(self.name))
+        raise Exception('start port method is not implemented by this switch %s', self.name))
 
     def restart_port(self, port, seconds=0):
-        raise Exception('restart port method is not implemented by this switch {}'.format(self.name))
+        raise Exception('restart port method is not implemented by this switch %s', self.name))
