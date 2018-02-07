c2b06cb2376e6a1d51fb803fdb85f5c5f64aff75
qasimraz <qasimraza788@gmail.com>
2018-02-07 11:05:17 +1100

    added logging

--- flowmanager/topology.py
+++ flowmanager/topology.py
@@ -49,6 +49,7 @@ class Topology(object):
                     new_switch = OVS(properties, True)
 
                 self.add_switch(new_switch)
+                logging.info(self.switches.values())
 
         self.controllers = {}
         self.ctrl_name = None
